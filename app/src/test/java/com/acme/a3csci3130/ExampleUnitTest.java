f8ffae4953d6ef0a707bf4a87eddc5359eaffcbd
Samblam <sam.barefoot@dal.ca>
2018-03-15 22:46:26 -0300

    Firerules finished, all CRUD functions operate. Project Finished

--- app/src/test/java/com/acme/a3csci3130/ExampleUnitTest.java
+++ app/src/test/java/com/acme/a3csci3130/ExampleUnitTest.java
@@ -1,5 +1,11 @@
 package com.acme.a3csci3130;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +17,34 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void Read() throws Exception {
+        /**
+         
+         */
+        FirebaseDatabase database;
+        DatabaseReference myRef;
+        database = FirebaseDatabase.getInstance();
+       String check;
+       final int checker;
+        myRef = database.getReference("businesses/-L7gLnazvLryRSCVdWQL/name");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() == "test dont touch"){
+                    assertTrue(true == true);
+                }
+                else{
+                    assertTrue(false == true);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 }
